700a690e5df0c9c138a3e07b248fb0d82f51f918
evoskoboeva <GITHUB evoskoboeva@tango.com.ua>
2021-02-03 00:52:11 +0200

    home task lesson3

--- src/com/companyCat/Main.java
+++ src/com/companyCat/Main.java
@@ -10,62 +10,180 @@ public class Main {
         //HomeTask1();
         //HomeTask2();
         //HomeTask3();
+        //HomeTask4();
         //HomeTask5();
+        //HomeTask6();
         //HomeTask7();
-        HomeTask11();
+        //HomeTask8();
+        //HomeTask9();
+        HomeTask10();
+        //по рисованию геометрических фигур прошу не судить строго  - мало что в памяти из геометрии.
+        // на ромб не хватило усидчивости(
+        //HomeTask11_FourAngles();
+        //HomeTask11_Three90Angles();
+        //HomeTask11_Three60Angles();
+
+    }
+
+    private static void HomeTask10() {
+        /*10) Дана арифметическая прогрессия 1, 5, 9, 13, ... Написать программу
+        определения суммы первых десяти ее членов. Вывести значение суммы и всех ее
+        слагаемых.
+        */
+
+        Scanner scanner = new Scanner(System.in);
+        double sumA = 0;
+        int countA=0;
+        System.out.println("Введите параметры арифметической прогрессии с которой будем играть A (1) и B(4):");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("Введите количество членов арифметической прогрессии N (10)");
+        int n = scanner.nextInt();
+        for (int i = a; countA < n ; i+=b) {
+            sumA +=i;
+            countA++;
+            System.out.println("Номер "+ countA+ " - член арифм прогрессии "+i);
+                    }
+        System.out.println("Сумма первых "+countA+" членов прогрессии = "+sumA);
+    }
+
+    private static void HomeTask9() {
+        //9) Дана арифметическая прогрессия с параметрами а = 2, b = 4. Сколько нужно
+        //взять членов прогрессии, начиная с первого, чтобы их сумма превысила
+        //заданную величину Z. Написать программу решения задачи. Вывести искомое
+        //число слагаемых, величину суммы и последнее слагаемое.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите Z");
+        double Z = scanner.nextDouble();
+        int i=0, a=2, b= 4, count=0, lastI=0;
+        double sumI = 0;
+        for (i = a; sumI <=Z ; i+=b) {
+            sumI +=i;
+            count++;
+            lastI=i;
+       }
+        System.out.println("искомое число слагаемых = "+count +"  величинa суммы  = "+sumI +
+                "   и последнее слагаемое = " +lastI);
+    }
+
+    private static void HomeTask8() {
+        //8) Найти 20 первых натуральных чисел, делящихся нацело на 13 или на 17 и
+        //больших 500.
+        int count=0;
+        for (int i = 501; count <20 ; i++) {
+            if ( i % 13 == 0 || i % 17 == 0){
+                count++;
+                System.out.println("Номер "+count+"  Делится на 13 или 17 = "+i);
+            }
+        }
+    }
+
+    private static void HomeTask6() {
+        //6) Составить программу для нахождения всех натуральных решений (х и у)
+        //уравнения х^2+y^2=k^2
+        //, где х, у и k лежат в интервале от 1 до 30. Решения, которые
+        //получаются, перестановкой х и у, считать совпадающими.
+        int  y = 1, k = 1;
+        double x = 1;
+        for (k = 1; k<= 30; k++){
+            for (y = 1; y <=30;y++){
+                x = Math.sqrt(k*k-y*y);
+                    if (x %2 ==0 && x !=0){
+                    System.out.println(" x = "+x+ ";  y = "+y+";  k = "+k);
+                    break; // выходим из цикла k как только найден0 хоть одно решение
+                }
+
+            }
+        }
+    }
+
+    private static void HomeTask4() {
+        //вычислить значение фунции Z = sum (i=1:20) sum (k=1,n) X^k(i)/k!
+        //Xi ввод с клавиатуры
+        //k=1,2,..n
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение n для фунции Z = sum (i=1:20) sum (k=1,n) X^k(i)/k!");
+        int n= scanner.nextInt();
+        double x=0, z=0;
+        for (int i = 1; i <=20 ; i++) {
+            System.out.println("Введите значение X для фунции Z = sum (i=1:20) " +
+                    "sum (k=1,n) X^k(i)/k! для итерации номер " +i);
+            x = scanner.nextDouble();
+            for (int k=1;k<=n;k++){
+                z+= Math.pow(x, k)/factorial(k);
+                //System.out.println(k+"  z  = "+z); Проверка как циклы ходят
+
+            }
+        }
+        System.out.println("z = "+z);
 
 
     }
 
-    private static void HomeTask11() {
+    private static void HomeTask11_Three60Angles() {
         //11) Напишите программы, которые выводят на экран :
-        // прямоугольник
-        // прямоугольный треугольник
         // равносторонний треугольник
-        // ромб
-        int a = 25;
-        int b = 10;
-        int j = 0, i = 0;
-        for (i = 0; i < a; i++) {
-            for (j = 0; j < b; j++) {
-                System.out.print(" * ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сторону равностороннего треугольника  a =  * *");
+        double a = scanner.nextDouble();
+        double h = 0;
+        h = (Math.sqrt(3)*a)/2; //точно не уверенна )) это высота такого треугольника
 
-            }
-            System.out.println(System.lineSeparator());
-        }
-        System.out.println("------------------------------");
-        for (i = 0; i < a; i++) {
-            for (j = 0; j < b; j++) {
-                System.out.print(" * ");
+        for (int i = 0; i <= h; i++) {
 
-            }
-            b--;
-            System.out.println(System.lineSeparator());
-        }
-
-
-        System.out.println("------------------------------");
-        for (i = 0; i < a; i++) {
-
-
-            for (int k = 1; k < a/2; k++) {
+            for (int k = 1; k <= h; k++) {
                 if (k <= i) {
                     System.out.print("   ");
                 } else {
-            System.out.print(" * ");
-        }
+                    System.out.print(" * ");
+                }
             }
 
-            for (j = 0; j < a/2; j++) {
+            for (int j = 1; j <= (a)+1; j++) {
                 System.out.print(" * ");
-
             }
             a--;
             System.out.println(System.lineSeparator());
         }
 
+
     }
 
+    private static void HomeTask11_Three90Angles() {
+        //11) Напишите программы, которые выводят на экран :
+        // прямоугольный треугольник
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите меньший катет прямоугольного треугольника");
+        int a = scanner.nextInt();
+        double b = a*1.2; // просто формально больший катет
+        int j = 0, i = 0;
+
+        for (i = 1; i <= b; i++) {
+            for (j = 1; j <= a; j++) {
+                System.out.print(" * ");
+            }
+            a--;
+            System.out.println(System.lineSeparator());
+        }
+    }
+
+    private static void HomeTask11_FourAngles() {
+        //11) Напишите программы, которые выводят на экран :
+        // прямоугольник
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите стороны прямоугольника a (высота) и b(ширина):");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int j = 0, i = 0;
+
+        for (i = 0; i < a; i++) {
+            for (j = 0; j < b; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println(System.lineSeparator());
+        }
+    }
 
 
 private static void HomeTask7() {
